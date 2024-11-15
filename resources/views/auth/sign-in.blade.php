a06e3e223b5bbed90ca4574e4f61c30de6253c34
Vasyl Khatsiur <khatsiur.vasyl@homsters.com>
2024-11-15 21:02:05 +0200

    add `required` attribute to inputs of  auth forms

--- resources/views/auth/sign-in.blade.php
+++ resources/views/auth/sign-in.blade.php
@@ -2,8 +2,8 @@
 <x-layout>
     <x-forms.form class="flex flex-col mt-12 p-8 pt-0" method="POST" action="{{ route(RouteNames::STORE_SIGN_IN) }}">
         <h1 class="font-bold text-center text-4xl mb-8">Sign in to TV Show</h1>
-        <x-forms.input label="Email" name="email" type="email" />
-        <x-forms.input label="Password" name="password" type="password" placeholder="" />
+        <x-forms.input label="Email" name="email" type="email" required />
+        <x-forms.input label="Password" name="password" type="password" required />
 
         <x-forms.button>Sign In</x-forms.button>
     </x-forms.form>
--- resources/views/auth/sign-up.blade.php
+++ resources/views/auth/sign-up.blade.php
@@ -2,10 +2,10 @@
 <x-layout>
     <x-forms.form class="flex flex-col mt-12 p-8 pt-0" method="POST" action="{{ route(RouteNames::STORE_SIGN_UP) }}">
         <h1 class="font-bold text-center text-4xl mb-8">Sign up on TV Show</h1>
-        <x-forms.input label="Name" name="name" />
-        <x-forms.input label="Email" name="email" type="email" />
-        <x-forms.input label="Password" name="password" type="password" />
-        <x-forms.input label="Password Confirmation" name="password_confirmation" type="password" />
+        <x-forms.input label="Name" name="name" required />
+        <x-forms.input label="Email" name="email" type="email" required />
+        <x-forms.input label="Password" name="password" type="password" required/>
+        <x-forms.input label="Password Confirmation" name="password_confirmation" type="password" required/>
 
         <x-forms.button>Sign Up</x-forms.button>
     </x-forms.form>
