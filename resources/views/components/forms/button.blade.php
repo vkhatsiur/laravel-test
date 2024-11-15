5a5f44334195a90d47b397f14088ee1f87a1d8ea
Vasyl Khatsiur <khatsiur.vasyl@homsters.com>
2024-11-15 21:01:20 +0200

    add click effect to form.button

--- resources/views/components/forms/button.blade.php
+++ resources/views/components/forms/button.blade.php
@@ -1,1 +1,1 @@
-<button {{ $attributes(['class' => 'bg-pink-400 mx-auto rounded py-2 px-6 font-bold']) }}>{{ $slot }}</button>
+<button {{ $attributes(['class' => 'bg-pink-400 mx-auto rounded py-2 px-6 font-bold hover:bg-pink-500 active:bg-pink-600 text-white']) }}>{{ $slot }}</button>
